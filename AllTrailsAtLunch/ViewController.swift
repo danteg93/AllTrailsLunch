2c5bb9486f8e86092da1cdbc3c8ace3bdea0257e
Dante Garcia <dantegarciaram93@gmail.com>
2022-05-14 14:34:16 -0700

    Initial Commit

---
+++ AllTrailsAtLunch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AllTrailsAtLunch
+//
+//  Created by Dante Garcia on 5/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
