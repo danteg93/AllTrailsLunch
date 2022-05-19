08bb580cae26853c1a0037afcbc93c9f7a6f0ca0
Dante Garcia <dantegarciaram93@gmail.com>
2022-05-19 11:16:14 -0700

    * List will display data from the API

--- AllTrailsAtLunch/Presentation/Discovery/ListView/ListViewController.swift
+++ AllTrailsAtLunch/Presentation/Discovery/ListView/ListViewController.swift
@@ -8,37 +8,68 @@
 import Foundation
 import UIKit
 
-class ListViewController: LayoutReadyViewController {
+class ListViewController: LayoutReadyViewController, Displayable {
+        
+    typealias State = ListViewState
+    typealias Presenter = ListViewPresenter
     
     enum Constants {
         static let listCellIdentifier = "ListTableViewCell"
+        static let tableBottomInset: CGFloat = 85.0
+        static let cellHeight: CGFloat = 150.0
     }
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet private weak var tableView: UITableView!
+    
+    var presenter: ListViewPresenter?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.createPresenter()
         self.tableView.register(UINib(nibName: Constants.listCellIdentifier, bundle: nil), forCellReuseIdentifier: Constants.listCellIdentifier)
+    }
+    
+    override func viewIsReady() {
+        super.viewIsReady()
+        self.tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: Constants.tableBottomInset, right: 0)
+        self.presenter?.setup()
+    }
+    
+    func display(_ displayableState: DisplayableState) {
+        switch displayableState {
+        case .stateNotRequested:
+            break
+        case .empty:
+            break
+        case .loading:
+            break
+        case .error:
+            break
+        case .populated:
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
     
 }
 
 extension ListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        3
+        self.presenter?.viewModel.places.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.listCellIdentifier, for: indexPath)
-        if let listCell = cell as? ListTableViewCell {
-            let model = PlaceCardModel(titleLabel: "\(indexPath.row)")
+        if let listCell = cell as? ListTableViewCell, let place = self.presenter?.viewModel.places[indexPath.row] {
+            let model = PlaceCardModel(titleLabel: place.name ?? "")
             listCell.setup(model)
         }
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 150.0
+        return Constants.cellHeight
     }
     
 }
--- AllTrailsAtLunch/Presentation/Discovery/ListView/ListViewPresenter.swift
+++ AllTrailsAtLunch/Presentation/Discovery/ListView/ListViewPresenter.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import Combine
+
+struct ListViewState {
+    
+    var places: [PlaceEntity] = []
+}
+
+class ListViewPresenter: Presentable {
+    
+    var viewModel = ListViewState()
+    var error: Error?
+    var display: DisplayLogic
+    
+    private var placesCacheObserver: AnyCancellable?
+    
+    required init(display: @escaping DisplayLogic) {
+        self.display = display
+    }
+    
+    func setup() {
+        self.placesCacheObserver = NearbyRestaurantsEntity.subscribe { [weak self] (result) in
+            switch result {
+            case .success(let entity):
+                self?.viewModel.places = entity.results
+                self?.display(.populated)
+            case .failure:
+                break
+            }
+        }
+    }
+    
+    deinit {
+        self.placesCacheObserver?.cancel()
+    }
+}
